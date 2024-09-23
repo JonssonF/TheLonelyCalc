dd1d12c9adc450fa13cce690c581fc4adcb7e238
Fredrik Jonsson <Fredrikandrejonson@hotmail.com>
2024-09-23 13:23:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TheLonelyCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
